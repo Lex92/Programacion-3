e59d0724ca91a2205d764858b8f8a897caf0d8a2
Lex92 <l.libarona@gmail.com>
2015-07-01 19:24:44 -0300

    Día2: Agregado randomizador de colores (pelo y gorra), con intención de testear Save/Load en futuro próximo

---
+++ UNITY/Assets/Scripts/TintColor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TintColor : MonoBehaviour {
+	
+	public void ChangeColor (Color c) {
+		GetComponent<SpriteRenderer>().material.color = c;
+	}
+}
--- UNITY/Assets/Scripts/v2/Character/CharacterMain.cs
+++ UNITY/Assets/Scripts/v2/Character/CharacterMain.cs
@@ -6,13 +6,36 @@ public class CharacterMain : MonoBehaviour {
 	public Direction dir = Direction.down;
 	//public bool canMove = true;
 	public string charName = "default";
-	
+	public Color hairTint;
+	public Color hatTint;
 	// move "graphical user interface", gets the movement from screen interaction
 	protected MoveGUI moveGui;
 	
 	void Start(){
+		int i;
 		moveGui = GameObject.Find("MoveGUIGO").GetComponent<MoveGUI>();
 		Input.simulateMouseWithTouches = true;
+		
+		TintColor[] colores = GetComponentsInChildren<TintColor>(true);
+		
+		hairTint.a=1;
+		hairTint.b=Random.Range(0.0f,1.0f);
+		hairTint.r=Random.Range(0.0f,1.0f);
+		hairTint.g=Random.Range(0.0f,1.0f);
+		hatTint.a=1;
+		hatTint.b=Random.Range(0.0f,1.0f);
+		hatTint.r=Random.Range(0.0f,1.0f);
+		hatTint.g=Random.Range(0.0f,1.0f);
+		
+		Debug.Log(colores.Length);
+		for(i=0;i<colores.Length;i++){
+			if(colores[i].name.StartsWith("Hair")){
+				colores[i].ChangeColor(hairTint);
+			}
+			if(colores[i].name.StartsWith("Hat")){
+				colores[i].ChangeColor(hatTint);
+			}
+		}
 	}
 	void Update(){
 		//gets movement and saves direction
