c0cf8de0ca725f80a8a5d0158944d79d5d5542ac
Lex92 <l.libarona@gmail.com>
2015-07-01 08:51:22 -0300

    Día1: último commit del día, leves modificaciones del boceto de la batalla, comentarios de la misma explayados.

--- UNITY/Assets/Scripts/Battle.cs
+++ UNITY/Assets/Scripts/Battle.cs
@@ -2,13 +2,13 @@
 using System.Collections;
 
 public enum Stage{
-	inicio, elegir, cambio, item, atq1, atq2, resultado
+	inicio, elegir, cambio, item, atq1, atq2, atq3, resultado
 }
 
 public class Battle : MonoBehaviour {
 
 	public GameObject monst1,monst2;
-	public int battleStage = 0;
+	public int battleStage = (int)Stage.inicio;
 	
 	// Use this for initialization
 	void Start () {
@@ -19,28 +19,36 @@ public class Battle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		switch(battleStage){
-		case Stage.inicio:
 		battleStage++;
+		Debug.Log(battleStage);
+		switch(battleStage){
+		case (int)Stage.inicio:
+			//esto en realidad lo hace en Start()
 			break;
-		case Stage.elegir:
+		case (int)Stage.elegir:
 			//elegir accion
+			// instantiate menu(opciones-> atacar, item, cambiar, escapar)
 			break;
-		case Stage.cambio:
-			//en caso de pedirse un cambio
+		case (int)Stage.cambio:
+			//en caso de pedirse un cambio o escapar
 			break;
-		case Stage.item:
+		case (int)Stage.item:
 			//en caso de querer utilizar un item
 			break;
-		case Stage.atq1:
+		case (int)Stage.atq1:
 			//en caso de tener prioridad alta
 			break;
-		case Stage.atq2:
+		case (int)Stage.atq2:
 			//ataques normales
 			break;
-		case Stage.resultado:
+		case (int)Stage.atq3:
+			//ataques prioridad baja
+			break;
+		case (int)Stage.resultado:
+			battleStage = (int)Stage.inicio;
 			//al finalizar la batalla sale, si no vuelve a battleStage = Stage.elegir
 			break;
 		}
 	}
+	
 }
