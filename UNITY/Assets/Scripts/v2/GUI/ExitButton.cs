0ed1137840fcbebe0278484b1e1369bda8a54f7c
Lex92 <l.libarona@gmail.com>
2015-07-02 18:40:30 -0300

    Día3: Ligeros cambios en documentación, agregados estéticos (nuevos bocetos)

--- UNITY/Assets/Scripts/v2/GUI/ExitButton.cs
+++ UNITY/Assets/Scripts/v2/GUI/ExitButton.cs
@@ -10,7 +10,7 @@ public class ExitButton : MonoBehaviour {
 	void OnGUI(){
 
 		// exit button will only be avaiable on unity, as it has no own "exit button"
-		#if UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE_LINUX || UNITY_WP8
+		/*#if UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE_LINUX || UNITY_WP8
 		if(exitEnabled){
 			if(!exitTexture1){
 				if (GUI.Button(new Rect(Screen.width-60, 10, 50, 50), "EXIT"))
@@ -22,7 +22,18 @@ public class ExitButton : MonoBehaviour {
 				Application.Quit();
 			}
 		}
-		#endif
+		#endif*/
+		if((Screen.fullScreen)&& exitEnabled){
+			if(!exitTexture1){
+				if (GUI.Button(new Rect(Screen.width-60, 10, 50, 50), "EXIT"))
+					//Debug.Log("Clicked the button EXIT");
+					Application.Quit();
+			}else if (GUI.Button(new Rect(Screen.width-(10+Screen.width/10), 10, Screen.height/5, Screen.height/5), exitTexture1,GUIStyle.none)){
+				//Debug.Log("Clicked the button with an image EXIT");
+				exitTexture1 = exitTexture2;
+				Application.Quit();
+			}
+		}
 	}
 	/*
 	//Save before quitting
