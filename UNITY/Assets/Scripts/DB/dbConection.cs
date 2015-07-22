e196571a3ae828b34f27c4b4d08d3745de0b7d57
Lex92 <l.libarona@gmail.com>
2015-07-22 15:02:54 -0300

    algunas modificaciones de las DB, todavia no corre en android (pero si crea una en caso de no existir en windows)

--- UNITY/Assets/Scripts/DB/dbConection.cs
+++ UNITY/Assets/Scripts/DB/dbConection.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using Mono.Data.Sqlite;
 
-public class dbConection : MonoBehaviour {
+public class dbConection {
 	private SqliteConnection _conexion;
 	private SqliteCommand _command;
 	private SqliteDataReader _reader;
@@ -16,30 +16,38 @@ public class dbConection : MonoBehaviour {
 	}
 
 	public void SelectData(){
-		_query = "Select * From continar where name='PEPE'";
+		_query = "Select * From continar where name='PEPE'";//continar?
 		_command = _conexion.CreateCommand ();
 		_command.CommandText = _query;
 		_reader = _command.ExecuteReader ();
 
 		if(_reader != null){
 			while(_reader.Read()){
-				print(_reader.GetValue(1).ToString() + " - " + _reader.GetValue(2).ToString() );
+				Debug.Log(_reader.GetValue(1).ToString() + " ; " + _reader.GetValue(2).ToString() );
 				PlayerPrefs.SetFloat("positionX",float.Parse(_reader.GetValue(1).ToString()));
 				PlayerPrefs.SetFloat("positionY",float.Parse(_reader.GetValue(2).ToString()));
 
 			}
 		}
 	}
+	
+	public void CrearTabla(string tabla){
+		_query = "CREATE TABLE "+tabla+"(name CHAR(20) NOT NULL, posicionX CHAR (20), posicionY CHAR(20));";
+		_command = _conexion.CreateCommand();
+		_command.CommandText = _query;
+		_command.ExecuteReader();
+		InsertData("0","0");
+	}
 
 	public void InsertData(string posx,string posy){
-		_query = "INSERT INTO continar VALUES('PEPE','"+posx+"','"+ posy +"')";
+		_query = "INSERT INTO continar VALUES('PEPE','"+posx+"','"+ posy +"')";//continar?
 		_command = _conexion.CreateCommand ();
 		_command.CommandText = _query;
 		_command.ExecuteReader ();
 	}
 
 	public void UpdateData(string posx, string posy){
-		_query = "UPDATE continar SET posicionX='"+posx+"',posicionY='"+posy+"' where name='PEPE'" ;
+		_query = "UPDATE continar SET posicionX='"+posx+"',posicionY='"+posy+"' where name='PEPE'" ;//continar?
 		_command = _conexion.CreateCommand ();
 		_command.CommandText = _query;
 		_command.ExecuteReader ();
--- UNITY/Assets/Scripts/DB/sqlScript.cs
+++ UNITY/Assets/Scripts/DB/sqlScript.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 
 public class sqlScript : MonoBehaviour {
-	public string URIDataBase;
+	public string URIDataBase = "";
+	
+	public string databaseName = "DBase";
 	
 	public dbConection _connector;
 
 	public dbConection conectarDB(){
 		_connector = new dbConection ();
-		_connector.OpenDB ("URI=file:C:\\Users\\Luk\\Documents\\Unlu\\Programacion 3\\Proyecto\\Repositorio\\trunk\\UNITY\\Assets\\DB\\DB");
+		
+		
+		if(Application.platform == RuntimePlatform.Android){
+			Debug.Log("1");
+			URIDataBase =Application.persistentDataPath + "/" + databaseName;
+			Debug.Log (URIDataBase);
+		}else{
+			URIDataBase = "Assets/"+databaseName;
+		}
+		if(!File.Exists(URIDataBase)){
+			Debug.Log("2");
+			string path = "jar:file://"+Application.dataPath+"!/assets/"+databaseName;
+			WWW loadDB = new WWW(path);
+			while(!loadDB.isDone){
+			}
+			Debug.Log("3");
+			Debug.Log(URIDataBase);
+			File.WriteAllBytes(URIDataBase,loadDB.bytes);
+			_connector.OpenDB("URI=file:"+URIDataBase);
+			Debug.Log("crear tabla");
+			_connector.CrearTabla("continar");
+			_connector.InsertData("0","0");
+		}else{
+			_connector.OpenDB("URI=file:"+URIDataBase);
+		}
 		return _connector;
 	}
 
